1a49a1a15c4ecffe6aca694cf5ffeb025241fca0
Chan Chhaya <chhayajr10@gmail.com>
2020-08-08 08:09:15 +0700

    Add BasicAuthentication & Permit OpenApi URL

---
+++ src/main/java/com/chhaya/amsapi/configuration/CustomBasicAuthenticationEntryPoint.java
@@ -0,0 +1,32 @@
+package com.chhaya.amsapi.configuration;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Component
+public class CustomBasicAuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+
+        response.addHeader("WWW-Authenticate", "Basic realm=" + getRealmName());
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        PrintWriter writer = response.getWriter();
+        writer.println("HTTP Status 401 - " + authException.getMessage());
+
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        setRealmName("chanchhaya");
+    }
+
+}
---
+++ src/main/java/com/chhaya/amsapi/configuration/SecurityConfiguration.java
@@ -0,0 +1,56 @@
+package com.chhaya.amsapi.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private CustomBasicAuthenticationEntryPoint customBasicAuthenticationEntryPoint;
+
+    @Autowired
+    public void setCustomBasicAuthenticationEntryPoint(CustomBasicAuthenticationEntryPoint customBasicAuthenticationEntryPoint) {
+        this.customBasicAuthenticationEntryPoint = customBasicAuthenticationEntryPoint;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable();
+
+        http.authorizeRequests()
+                .anyRequest().authenticated();
+
+        http.httpBasic()
+                .authenticationEntryPoint(customBasicAuthenticationEntryPoint);
+
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("chanchhaya")
+                .password("{noop}chanchhaya")
+                .roles("ADMIN");
+
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v3/api-docs/**",
+                "/swagger-ui.html",
+                "/swagger-ui/**");
+    }
+
+}
